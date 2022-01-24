4a439aa5c9d170af6868e2f9431abdf24dac4c0b
Roger Karlsen <mr.haggbart@gmail.com>
2022-01-24 16:06:25 +0100

    init

---
+++ app/src/main/java/com/haggbart/dat153/namequiz/DatabaseActivity.java
@@ -0,0 +1,14 @@
+package com.haggbart.dat153.namequiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DatabaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_database);
+    }
+}
---
+++ app/src/main/java/com/haggbart/dat153/namequiz/MainActivity.java
@@ -0,0 +1,35 @@
+package com.haggbart.dat153.namequiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnQuiz = findViewById(R.id.btnQuiz);
+        Button btnDatabase = findViewById(R.id.btnDatabase);
+
+        btnQuiz.setOnClickListener(this);
+        btnDatabase.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        Log.d(TAG, "onClick: Button clicked: " + view.getResources().getResourceEntryName(view.getId()));
+        Intent intent = new Intent(this, DatabaseActivity.class);
+
+        startActivity(intent);
+    }
+}
